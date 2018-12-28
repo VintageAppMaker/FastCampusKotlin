efb70ab0c8baaffdaee724e1b7d46d4507a282c1
Vintage appMaker <adsloader99@gmail.com>
2018-12-28 10:30:53 +0900

    .

--- app/src/main/java/com/psw/crack/fastcampuskotlin/javainterlop/javaActivity.java
+++ app/src/main/java/com/psw/crack/fastcampuskotlin/javainterlop/javaActivity.java
@@ -1,16 +1,23 @@
 package com.psw.crack.fastcampuskotlin.javainterlop;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Toast;
 import com.psw.crack.fastcampuskotlin.R;
 
 // static하게 import한다.
-import static com.psw.crack.fastcampuskotlin.javainterlop.JavaActivitytKt.kotlinEventHandler;
+import static com.psw.crack.fastcampuskotlin.javainterlop.KotlinActivitytKt.kotlinEventHandler;
 
 public class javaActivity extends AppCompatActivity {
 
     String sMessage = "Hi, Java";
+
+    // kotlin에서 사용가능
+    public void T(Context ctx, String sMessage){
+        Toast.makeText(ctx, sMessage, Toast.LENGTH_LONG).show();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,8 +36,8 @@ public class javaActivity extends AppCompatActivity {
         // class가 아닌 변수와 함수는 자동으로 변환한다.
         // 파일명을 대소문자 변경 후, 뒤에 Kt를 붙인다.
         // 그리고 . 연산자 이후에 사용가능
-        KotlinTestKt.MyTest();
-        KotlinTestKt.getName();
+        KotlinActivitytKt.getName();
+        KotlinActivitytKt.MyTest();
 
         // 클래스명은 그대로 사용가능
         // 어노테이션으로 자바에서 사용할 것을 지정해주어야 함.
--- app/src/main/java/com/psw/crack/fastcampuskotlin/javainterlop/kotlinActivityt.kt
+++ app/src/main/java/com/psw/crack/fastcampuskotlin/javainterlop/kotlinActivityt.kt
@@ -1,6 +1,21 @@
 package com.psw.crack.fastcampuskotlin.javainterlop
 
 import android.util.Log
+import kotlinx.android.synthetic.main.activity_java.*
+import java.util.*
+
+// java로 만든 클래스에서도 확장함수 추가가 가능하다!!
+fun javaActivity.kotlinEventHandler(){
+    // javaActivity에서 선언한 멤버필드
+    val s  = sMessage
+
+    // javaActivity에서 정의한 Control
+    btnOk.setOnClickListener {
+
+        // javaActivity의  T 메소드 사용
+        T(applicationContext, "${s} ${Date().toString()}")
+    }
+}
 
 class KotlinExport{
     companion object {
@@ -24,5 +39,6 @@ class KotlinExport{
 }
 
 // Kotlin 일반함수와 변수
+// 파일명Kt.class 형식으로 생성된다(대소문자 변경유의).
 var name = "msg"
 fun MyTest(){}
